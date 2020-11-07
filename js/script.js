a1adc18969223812c4d7a6b5e59aefe4c686007d
Alexei <donttryhackmyemail@gmail.com>
2020-11-07 19:13:48 +0300

    Added dir js help .js

---
+++ js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+// alert("Hello");
+const result = confirm("Are you here?");
+console.log(result);
+
+const answer = prompt(" your age is 18?")
+
+console.log('asdasd'+ "ddffdf");
+console.log(4 + +"5");
+
+let incr=10,
+    decr=10;
+
+incr++;
+decr--;
+console.log(incr);
+console.log(decr);
+
+let incr=10,
+    decr=10;
+
+++incr;
+--decr;
+console.log(incr);
+console.log(decr);

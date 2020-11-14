b982e83ba7f2a4b1eded477c4d08e4e221d436a0
Alexei <donttryhackmyemail@gmail.com>
2020-11-14 19:59:01 +0300

    commit N3

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@
 const result = confirm("Are you here?");
 console.log(result);
 
-const answer = prompt(" your age is 18?")
+const answer = prompt(" your age is 18?");
 
 console.log('asdasd'+ "ddffdf");
-console.log(4 + +"5");
+console.log(4 + + "5");
 
 let incr=10,
     decr=10;
@@ -16,8 +16,6 @@ decr--;
 console.log(incr);
 console.log(decr);
 
-let incr=10,
-    decr=10;
 
 ++incr;
 --decr;

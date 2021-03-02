1c636813342a21c72442eb57f20e625c6875428e
alter <donttryhackmyemail@gmail.com>
2021-03-03 00:00:44 +0300

    2 part of task added

--- script.js
+++ script.js
@@ -67,7 +67,16 @@ personalMovieDB.movies[c] = d;*/
 for (let i=0;i<2;i++){
     const a = prompt('Один из последних просмотренных фильмов?', ''),
           b = prompt('На сколько оцените его?', '');
-    1personalMovieDB.movies[a] = b;
+    if(a != null && b != null && a != "" && b != "" && a.length < 50){
+        personalMovieDB.movies[a] = b;
+        console.log('done!');
+    }
+    else{
+        console.log('error, repeat answer, please');
+        i--;
+    }
+    
+          
 }
 
 console.log(personalMovieDB);
